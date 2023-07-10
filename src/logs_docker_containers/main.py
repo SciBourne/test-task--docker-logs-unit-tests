7636c4b6bba65da73fc2565840bc0787b8a4eced
SciBourne <bourne-sci-hack@yandex.ru>
2023-07-10 20:15:52 +0500

    FEAT: Function logs(container: str, name: str) -> None

---
+++ src/logs_docker_containers/main.py
@@ -0,0 +1,13 @@
+import aiohttp
+
+
+async def logs(container: str, name: str) -> None:
+    connection = aiohttp.UnixConnector(path="/var/run/docker.sock")
+
+    async with aiohttp.ClientSession(connector=connection) as session:
+        path = f"http://xx/containers/{container}/logs"
+        query = {"follow": 1, "stdout": 1}
+
+        async with session.get(url=path, params=query) as response:
+            async for line in response.content:
+                print(name, line)
